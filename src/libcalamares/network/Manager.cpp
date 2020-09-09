b1cedaf6b9915b0716575e70faba721bd48c64ed
demmm <anke62@gmail.com>
2020-09-09 10:35:24 +0200

    reverting https://github.com/calamares/calamares/commit/c6a3e9b81669c8c15237bfba329bf0f894df5380 check if that is the cause of localeq hasInternet crash

--- src/libcalamares/network/Manager.cpp
+++ src/libcalamares/network/Manager.cpp
@@ -149,23 +149,18 @@ Manager::hasInternet()
 bool
 Manager::checkHasInternet()
 {
+    bool hasInternet = d->nam()->networkAccessible() == QNetworkAccessManager::Accessible;
 
-    d->m_hasInternet = synchronousPing( d->m_hasInternetUrl );
-
-// For earlier Qt versions (< 5.15.0), set the accessibility flag to
-// NotAccessible if synchronous ping has failed, so that any module
-// using Qt's networkAccessible method to determine whether or not
-// internet connection is actually avaialable won't get confused over
-// virtualization technologies.
-#if ( QT_VERSION < QT_VERSION_CHECK( 5, 15, 0 ) )
-    if ( !d->m_hasInternet )
+    if ( !hasInternet && ( d->nam()->networkAccessible() == QNetworkAccessManager::UnknownAccessibility ) )
     {
-        d->nam()->setNetworkAccessible( QNetworkAccessManager::NotAccessible );
+        hasInternet = synchronousPing( d->m_hasInternetUrl );
     }
-#endif
-
-    emit hasInternetChanged( d->m_hasInternet );
-    return d->m_hasInternet;
+    if ( hasInternet != d->m_hasInternet )
+    {
+        d->m_hasInternet = hasInternet;
+        emit hasInternetChanged( hasInternet );
+    }
+    return hasInternet;
 }
 
 void
