d12adf8ec2e1bbea96baa83af9382d4d596bae50
demmm <anke62@gmail.com>
2021-10-06 13:17:49 +0200

    merge transifex cala branding, adding da

--- src/branding/kaos_branding/lang/calamares-kaos_branding_da.ts
+++ src/branding/kaos_branding/lang/calamares-kaos_branding_da.ts
@@ -1,35 +1,35 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="da" version="2.1">
 <context>
     <name>show</name>
     <message>
         <location filename="../show.qml" line="48"/>
         <source>Here the actual install of KaOS will start.&lt;br/&gt;Use the left &lt;b&gt;mouse button&lt;/b&gt; to go to the next slide, right for previous.&lt;br/&gt;After creating your chosen disk setup in the first 10 % &lt;br/&gt;the full copying of the ISO will take the longest of this install phase &lt;br/&gt;and will run until approximately 30%.&lt;br/&gt;</source>
-        <translation>Aquí es donde realmente comienza la instalación de KaOS.&lt;br/&gt;Use &lt;b&gt;el botón izquierdo del mouse&lt;/b&gt; para ir a la siguiente diapositiva, el derecho para la anterior.&lt;br/&gt;Luego de crearse la configuración de disco elegida durante el 10 % inicial,  &lt;br/&gt;la copia completa de la ISO será lo más largo de esta fase de la &lt;br/&gt;y se hará hasta aproximadamente el 30 %.&lt;br/&gt;</translation>
+        <translation>Her starter den egentlige installation af KaOS.&lt;br/&gt;Brug venstre &lt;b&gt;museknap&lt;/b&gt; for at gå til det næste dias, højre museknap for at gå til til forrige.&lt;br/&gt;Efter at have oprettet din valgte diskopsætning vil de første 10 % &lt;br/&gt; vil kopiering af ISO tage den længst tid i installationsfasen &lt;br/&gt;og vil fortsætte indtil ca. 30%.&lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../show.qml" line="74"/>
         <source>After the ISO is copied some 25 post-install modules will run.&lt;br/&gt;This includes setting user specific options, &lt;br/&gt;removing Live Session only packages&lt;br/&gt;and adjusting hardware setup.&lt;br/&gt;</source>
-        <translation>Luego de copiarse la ISO se ejecutarán unos 25 módulos de post-instalacion.&lt;br/&gt;Esto incluye la configuración de las opciones específicas de usuario.&lt;br/&gt;removiendo sólo los paquetes de la Sesión Live&lt;br/&gt;y ajustando la configuración del hardware.&lt;br/&gt;</translation>
+        <translation>Efter ISO-kopieringen kører omkring 25 moduler som de skal .&lt;br/&gt;Dette inkluderer indstilling af brugerspecifikke muligheder foretaget tidligere, &lt;br/&gt;fjernelse af pakker fra Live Session&lt;br/&gt;og justering af hardwareopsætning.&lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../show.qml" line="99"/>
         <source>The default Office Suite is Calligra.&lt;br/&gt;LibreOffice is available in the repositories. &lt;br/&gt;</source>
-        <translation>La Suite de Oficina por defecto es Calligra.&lt;br/&gt;LibreOffice se encuentra disponible en los repositorios.&lt;br/&gt;</translation>
+        <translation>Standard kontorpakken er Calligra.&lt;br/&gt;LibreOffice er tilgængelig i arkiverne. &lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../show.qml" line="122"/>
         <source>Qt/KDE specific internet applications include the &lt;br/&gt;Falkon web-browser and kde-telepathy for &lt;br/&gt;chat and Instant Messaging. &lt;br/&gt;</source>
-        <translation>Las aplicaciones Qt/KDE específicas de internet incluyen al &lt;br/&gt;navegador web Falkon y kde-telepathy para &lt;br/&gt;chat y Mensajería Instantánea. &lt;br/&gt;</translation>
+        <translation>Qt/KDE specifikke internet programmer omfatter &lt;br/&gt;Falkon web-browser og kde-telepathy for &lt;br/&gt;chat og Instant Messaging. &lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../show.qml" line="146"/>
         <source>For package management Octopi is the GUI application.&lt;br/&gt;</source>
-        <translation>Octopi es la aplicación GUI para la administración de paquetes.&lt;br/&gt;</translation>
+        <translation>Til pakkehåndtering er der Octopi til installation af programmer.&lt;br/&gt;</translation>
     </message>
     <message>
         <location filename="../show.qml" line="168"/>
         <source>May using KaOS be a pleasant experience for you.</source>
-        <translation>Que el uso de KaOS sea una experiencia placentera para Ud.</translation>
+        <translation>Vi håber at KaOS vil være en behagelig oplevelse for dig.</translation>
     </message>
 </context>
 </TS>
