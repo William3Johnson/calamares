5513e50903ba26df28bab00d8b4fc81d3a3c6b68
demmm <anke62@gmail.com>
2022-09-23 16:17:37 +0200

    with zfs replace the root= option line

--- src/modules/systemd-boot/main.py
+++ src/modules/systemd-boot/main.py
@@ -84,6 +84,7 @@ def create_conf(uuid, conf_path):
     swap_luks = ""
     cryptdevice_params = []
     btrfs_params = ""
+    zfs_params = ""
 
     for partition in partitions:
         if partition["fs"] == "linuxswap" and not "luksMapperName" in partition:
@@ -109,7 +110,7 @@ def create_conf(uuid, conf_path):
         if is_zfs_root(partition):
             zfs_root_path = get_zfs_root()
             if zfs_root_path is not None:
-                kernel_params.append("root=ZFS=" + zfs_root_path)
+                zfs_params = ("root=ZFS=" + zfs_root_path)
             else:
                 # Something is really broken if we get to this point
                 libcalamares.utils.warning("Internal error handling zfs dataset")
@@ -118,7 +119,10 @@ def create_conf(uuid, conf_path):
     if cryptdevice_params:
         kernel_params.extend(cryptdevice_params)
     else:
-        kernel_params.append("root=UUID={!s}".format(uuid))
+        if zfs_params:
+            kernel_params.append(zfs_params)
+        else:
+            kernel_params.append("root=UUID={!s}".format(uuid))
 
     if swap:
         kernel_params.append("resume=UUID={!s}".format(swap))
